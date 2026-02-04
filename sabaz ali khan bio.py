985af980d3f3921c8ea2419f0afc8a6e7018d874
MR Sabaz ALI Khan <sabazali236@gmail.com>
2026-02-04 02:36:58 -0800

    Add files via upload

---
+++ sabaz ali khan bio.py
@@ -0,0 +1,13 @@
+Mr. Sabaz Ali Khan is a passionate and dedicated Cyber Security Engineer with a strong interest in ethical hacking, network security, and defensive security strategies. He specializes in identifying system vulnerabilities, strengthening digital infrastructures, and promoting safe and responsible use of technology.
+
+With hands-on experience in Python scripting, penetration testing concepts, Kali Linux tools, and network security, Mr. Sabaz Ali Khan continuously works on developing security tools and learning modern cyber-defense techniques. He believes in using hacking skills ethically and legally to protect systems from cyber threats.
+
+Mr. Sabaz Ali Khan is highly motivated, self-driven, and always eager to explore emerging technologies in cybersecurity, automation, and digital forensics. His goal is to contribute to a safer cyber world by helping individuals and organizations secure their digital assets.
+
+Agar aap chahen to mai:
+
+GitHub profile sabaz ali khan and  Nike name Whoami592
+
+LinkedIn �About� Sabaz ali khan 
+
+Short YouTube  Name/ Sabaz ali khan hacking seris
